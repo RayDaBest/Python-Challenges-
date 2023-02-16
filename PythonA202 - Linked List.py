0c95625bfae452c61aedef81872d1438766ad97a
RayDaBest <112593963+RayDaBest@users.noreply.github.com>
2023-02-16 09:00:47 +0800

    Create PythonA202 - Linked List.py

---
+++ PythonA202 - Linked List.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
+    def find(self, data):
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == data:
+                return current_node
+            current_node = current_node.next
+        return None
+
+    def delete(self, data):
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current_node = self.head
+        while current_node.next is not None:
+            if current_node.next.data == data:
+                current_node.next = current_node.next.next
+                return
+            current_node = current_node.next
+
+    def print_list(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
